3f24d9f43856ea039e3a2769d23c54a40c6e4160
mcvdenk <mvdenk@gmail.com>
2016-01-23 12:00:20 +0100

    Implemented the force atlas layout

--- Plan/plan.js
+++ Plan/plan.js
@@ -1,4 +1,9 @@
-sigma.parsers.json('data.json', {
-  container: 'container'
+sigma.parsers.json(
+  'data.json', {
+  container: 'container',
+  },
+  function(s) {
+    // s.startForceAtlas2({worker: true, barnesHutOptimize: false});
 });
+
 
