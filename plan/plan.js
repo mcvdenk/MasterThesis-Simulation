ec09553af0cb9782e7942bda9c6ec481aba5cac5
mvdenk <mvdenk@gmail.com>
2016-01-23 09:17:10 -0500

    changed Plan to plan

---
+++ plan/plan.js
@@ -0,0 +1,43 @@
+//var DOTstring = 'dinetwork {1 -- 2; 2 -- 3; 2 -- 4 }';
+
+var container = document.getElementById('container');
+
+var parsedData;
+
+var network;
+
+var data;
+
+var options = {
+    interaction: {
+        dragNodes: false
+    },
+    layout: {
+        hierarchical: {
+            enabled: true,
+            levelSeparation: 150,
+            direction: 'LR'
+        }
+    }
+};
+
+var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+
+loadFile();
+
+function loadFile() {
+    reader.open('get', 'data.txt', true); 
+    reader.onreadystatechange = setupContents;
+    reader.send(null);
+}
+
+function setupContents() {
+    if(reader.readyState==4) {
+        parsedData = vis.network.convertDot(reader.responseText);
+        data = {
+            nodes: parsedData.nodes,
+            edges: parsedData.edges
+        };
+        network = new vis.Network(container, data, options);
+    }
+}
