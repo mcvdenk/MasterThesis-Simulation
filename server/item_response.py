50bb2fb283e127e1a5912bbc56dcd041485f4bf2
mvdenk <mvdenk@gmail.com>
2017-02-28 16:28:45 +0100

    End of the day commit

--- server/item_response.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import *
-from test_item import *
-
-class ItemResponse(Document):
-    connect('flashmap')
-    answer = StringField(default="")
-    item = ReferenceField(TestItem)
